da9f17eea60b1e69b4adf145f604b27079aaa248
Bogdannb34 <bogdan22bogdan@gmail.com>
2022-06-11 14:57:07 +0300

    Updates

---
+++ precache-manifest.d8e96f5d3a6e3bbecb410f897d1c978f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81fc660146fec62c85ab4d8679d189e",
+    "url": "/some-reactive-weather/index.html"
+  },
+  {
+    "revision": "7b89159f057d64ba8ae8",
+    "url": "/some-reactive-weather/static/css/2.862b2f9f.chunk.css"
+  },
+  {
+    "revision": "7b89159f057d64ba8ae8",
+    "url": "/some-reactive-weather/static/js/2.899b2903.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/some-reactive-weather/static/js/2.899b2903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b14d6ce56d9433e41a36",
+    "url": "/some-reactive-weather/static/js/main.4b0f2b83.chunk.js"
+  },
+  {
+    "revision": "80ba5de0aea2a140a6f4",
+    "url": "/some-reactive-weather/static/js/runtime-main.ee534886.js"
+  }
+]);
